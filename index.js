8bc63a6362e91a8cb6d72bb4dba48815695037a0
DenisKipchirchirMaiyo <dameanmasha649@gmail.com>
2025-04-02 11:08:45 +0300

    first push'

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+// index.js
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  // Export the functions so they can be accessed in the test file
+  module.exports = {
+    findMatching,
+    fuzzyMatch,
+    matchName
+  };
